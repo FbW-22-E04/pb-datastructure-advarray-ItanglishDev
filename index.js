8d207c89142461c18d79f6a1e52ba27241b808f7
chris <chrishethertonweb@gmail.com>
2022-08-18 15:36:20 +0200

    ex 2 done

---
+++ index.js
@@ -0,0 +1,21 @@
+// Ex 1
+
+const orders = [
+  { amount: 250 },
+  { amount: 400 },
+  { amount: 100 },
+  { amount: 325 }
+];
+
+
+
+
+
+
+
+// Ex 2
+
+const arrayOfNumbers = [3, 45, 6, 56, 7, 9];
+
+const newArrayOfNumbers = arrayOfNumbers.map(item => item + 1)
+console.log(newArrayOfNumbers);
